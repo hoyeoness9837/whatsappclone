2a8054c87096c8a347f4911db1d1289a06b7b383
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-09-07 21:43:37 -0700

    material icon package installed, default styles removed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
       <h1>What's App clone</h1>
+      <div className='app__body'>
+        {/* sidebar */}
+        <Sidebar />
+
+        {/* chat component */}
+        <Chat />
+      </div>
     </div>
   );
 }
