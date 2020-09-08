36df458eb4433dfa6c726d30657a3ee3148541db
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-09-07 21:43:56 -0700

    chat components and styling updated

---
+++ src/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Chat.css';
+
+
+function Chat() {
+  return (
+    <div className='chat'>
+      <h1> Im the chat</h1>
+    </div>
+  );
+}
+
+export default Chat;
